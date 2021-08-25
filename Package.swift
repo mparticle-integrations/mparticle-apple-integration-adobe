8fdcb9c8294e83f3f8efedbfbfe72fbb60ad7c74
Uday Alla <ualla@g.ucla.edu>
2021-08-25 14:02:55 -0400

    add package swift file

---
+++ Package.swift
@@ -0,0 +1,61 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-Adobe",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "mParticle-Adobe", targets: ["mParticle-Adobe"]), //, "mParticle-Adobe-Media"])
+        .library(name: "mParticle-Adobe-Media", targets: ["mParticle-Adobe-Media"])
+    ],
+    dependencies: [
+        .package(
+            name: "mParticle-Apple-SDK",
+            url: "https://github.com/mParticle/mparticle-apple-sdk",
+            .upToNextMajor(from: "8.0.0")),
+        .package(
+            name: "mParticle-Apple-Media-SDK",
+            url: "https://github.com/mParticle/mparticle-apple-media-sdk",
+            .upToNextMajor(from: "1.3.0")),
+        .package(
+            name: "AEPCore",
+            url: "https://github.com/adobe/aepsdk-core-ios.git",
+            .upToNextMajor(from: "3.2.0")),
+//        .package(
+//            name: "AEPIdentity",
+//            url: "https://github.com/adobe/aepsdk-core-ios.git",
+//            .upToNextMajor(from: "3.2.0")),
+//        .package(
+//            name: "AEPLifecycle",
+//            url: "https://github.com/adobe/aepsdk-core-ios.git",
+//            .upToNextMajor(from: "3.2.0")),
+//        .package(
+//            name: "AEPSignal",
+//            url: "https://github.com/adobe/aepsdk-core-ios.git",
+//            .upToNextMajor(from: "3.2.0")),
+        .package(name: "AEPUserProfile",
+            url: "https://github.com/adobe/aepsdk-userprofile-ios.git",
+            .upToNextMajor(from: "3.0.0")),
+        .package(name: "AEPAnalytics",
+            url: "https://github.com/adobe/aepsdk-analytics-ios.git",
+            .upToNextMajor(from: "3.0.0")),
+        .package(name: "AEPMedia",
+            url: "https://github.com/adobe/aepsdk-media-ios.git",
+            .upToNextMajor(from: "3.0.0"))
+
+    ],
+    targets: [
+        .target(
+            name: "mParticle-Adobe",
+            dependencies: ["mParticle-Apple-SDK"],
+            path: "mParticle-Adobe",
+            publicHeadersPath: "."),
+        .target(
+            name: "mParticle-Adobe-Media",
+            dependencies: ["mParticle-Apple-SDK", "mParticle-Apple-Media-SDK", "AEPCore", "AEPMedia", "AEPAnalytics", "AEPUserProfile"], //"AEPIdentity", "AEPLifecycle", "AEPSignal"],
+            path: "mParticle-Adobe-Media",
+            exclude: ["Dummy.swift"],
+            publicHeadersPath: "."),
+    ]
+)
