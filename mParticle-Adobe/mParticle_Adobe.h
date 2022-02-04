19a29629ba07f223e4fe7d1dc77971fc449c6729
bstalnaker <bstalnaker@mparticle.com>
2022-02-04 12:58:21 -0500

    fix: fix spm imports

--- mParticle-Adobe/mParticle_Adobe.h
+++ mParticle-Adobe/mParticle_Adobe.h
@@ -8,4 +8,8 @@ FOUNDATION_EXPORT const unsigned char mParticle_AdobeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <mParticle_Adobe/PublicHeader.h>
 
+#if defined(__has_include) && __has_include(<mParticle_Adobe/MPKitAdobe.h>)
 #import <mParticle_Adobe/MPKitAdobe.h>
+#else
+#import "MPKitAdobe.h"
+#endif
